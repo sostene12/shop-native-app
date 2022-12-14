1da62c4625a244456a04affb72fb05b509079317
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2022-12-14 11:15:07 +0200

    orders

--- components/OrderComponent.js
+++ components/OrderComponent.js
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
 
 const OrderComponent = ({order}) => {
+    console.log("products",order.products)
   return (
-    <View>
-      <Text>OrderComponent</Text>
+    <View style={styles.orderContainer}>
+        <View style={styles.order}>
+            {order.products.map(product => (
+                <View>
+                    <Text>{product.title}</Text>
+                    <Text>{product.quantity}</Text>
+                </View>
+            ))}
+            <Text>{order.amount}</Text>
+            <Text>{order.status}</Text>
+
+        </View>
     </View>
   )
 }
 
-export default OrderComponent
+export default OrderComponent;
+
+const styles = StyleSheet.create({
+    orderContainer:{
+        marginVertical:5
+    },
+    order:{
+        // flexDirection:'row',
+        // justifyContent:'space-between'
+    }
+});
