0957abc81ad1559977d03ecd414b875a44ec142b
sostene12 <ngarukiyimanasostene@gmail.com>
2024-01-07 16:21:34 +0200

    folder structure change

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { Provider} from "react-redux";
-import AllScreens from "./navigation/AllScreens";
-import store from "./redux/store"
+import AllScreens from "./app/navigation/AllScreens";
+import store from "./app/redux/store"
 
 export default function App() {
   // const cart = useSelector(state=> state.cart);
